7bbb959b07024c6156ad4120b8b1f01c8211aa0b
UzairRafq <157401656+UzairRafq@users.noreply.github.com>
2024-07-01 00:55:31 -0700

    Add files via upload

---
+++ script.py
@@ -0,0 +1,13 @@
+# print("Hello World") #this will print a string
+print("Uzair")
+print("Hello World")
+
+# #this line will print a number with + function
+print(3+4)
+
+#variables
+x = 3+4+5+7+8
+print(x)
+
+print(3+9)
+print("hello")
